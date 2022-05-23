37b3cca2a7c50b49676bad3954c6f2a188e155ae
click2cloud-lamda <raymond.mathew@cick2cloud.net>
2022-05-23 10:25:37 +0000

    Updated main.go for testing purpose

--- main.go
+++ main.go
@@ -20,6 +20,10 @@ func main() {
 	// fs := http.FileServer(http.Dir("build"))
 	// http.Handle("/", fs)
 	// var host string
+	var x=[]int{1,2,3,4,5,}
+	for _,z:=range x{
+		fmt.Println(z)
+	} 
 	fmt.Print("for test purpose007")
 	fmt.Println("localhost server starting check")
 	fmt.Println("Starting server on the port 8080...")
